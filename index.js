f4d88ca2c71e3b9b70d7e5036ef94e00aae4ba12
Asiak <asiakemp24@gmail.com>
2023-07-09 10:40:19 -0400

    pseudocoded what needed to be done

---
+++ index.js
@@ -0,0 +1,15 @@
+// Import required files/packages
+const inquirer = require('inquirer');
+const department = require('./department');
+const role = require('./roles');
+const employee = require('./employee');
+
+// Function for the main menu
+
+// Functions to view all the departments, roles, and employees
+
+// Function to add departments, roles, and employees
+
+// Function to update employee roles
+
+// code to start the application
