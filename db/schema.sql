ba3bf4ae5a314cb673457749ef4214f3ed9bf877
Asiak <asiakemp24@gmail.com>
2023-07-09 09:46:46 -0400

    created database and tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,25 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-
+    id INT NOT NULL,
+    department_name VARCHAR(30)
 );
 
 CREATE TABLE role (
+    id INT NOT NULL,
+    job_title VARCHAR (30),
+    department_name VARCHAR(30),
+    salary INT NOT NULL
 
 );
 
 CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    job_title VARCHAR (30),
+    department VARCHAR(30),
+    salary INT NOT NULL,
+    managers VARCHAR (30),
 
 );
