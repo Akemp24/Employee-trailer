911f52b00d30dd13c619805b8ba3d806b298188c
Asiak <asiakemp24@gmail.com>
2023-07-09 10:48:29 -0400

    created main menu choices

--- index.js
+++ index.js
@@ -5,7 +5,30 @@ const role = require('./roles');
 const employee = require('./employee');
 
 // Function for the main menu
-
+function mainMenu() {
+    inquirer.prompt([
+        {
+            type:'list',
+            message:What would you like to do?,
+            name:menuChoices,
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add A Department',
+                'Add A Role',
+                'Add An Employee',
+                'Update Employee Role',
+                'Exit'
+            ]
+        },
+    ])
+    .then((answer) => {
+        switch (answers.menuChoice) {
+            
+        }
+    })
+}
 // Functions to view all the departments, roles, and employees
 
 // Function to add departments, roles, and employees
